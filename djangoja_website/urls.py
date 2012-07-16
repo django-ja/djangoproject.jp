c15e88863a441a2d86a1083da7080ceb1ccbd808
hirokiky <hirokiky@gmail.com>
2012-07-16 19:12:27 +0900

    urls.py: DEBUG時の静的ファイル配信を追加

--- djangoja_website/urls.py
+++ djangoja_website/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,3 +11,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^m/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
