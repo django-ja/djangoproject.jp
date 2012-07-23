d0a0245a2f3d9855d8bebfcb64ee42aaad76fee4
hirokiky <hirokiky@gmail.com>
2012-07-24 07:52:39 +0900

    urls.py: ホームのビューをクラスベースに変更

--- djangoja_website/urls.py
+++ djangoja_website/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}, name="homepage"),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^weblog/', include('zinnia.urls')),
     url(r'^comments/', include('django.contrib.comments.urls')),
 
