dd190c5f27e2468381d3ac5cefb99eac153bdbc1
Hiroki KIYOHARA <hirokiky@gmail.com>
2015-04-01 21:38:32 +0900

    Fixed wsgi.py to solve path for djangoprojectjp.settings

--- djangoprojectjp/wsgi.py
+++ djangoprojectjp/wsgi.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 
 import os
+import sys
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
 settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
